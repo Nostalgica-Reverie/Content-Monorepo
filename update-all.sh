f638e11611821df8bc6efdc4edd9b78e1fdee951
omo50 <omo50@noreply.nostalgica.net>
2026-03-04 02:08:26 +0000

    chore(actions): attempt to make auto update for rc+ and simply

--- update-all.sh
+++ update-all.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo Updating
 (cd ./modpacks/simply-legacy/mr/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
+(cd ./modpacks/simply-legacy/cf/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
+(cd ./modpacks/re-console+/mr/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
+(cd ./modpacks/re-console+/cf/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
