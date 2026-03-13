dbdfbaa553948244da33580bd971f6f9dc8072c2
omo50 <144749186+omo50@users.noreply.github.com>
2026-03-13 14:02:01 -0600

    chore(actions): m,ake all use pw wrapper

--- refresh-all.sh
+++ refresh-all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Refreshing
-(cd ./modpacks/simply-legacy/mr/yarn/1.21.10-fabric && packwiz refresh -y) &
-(cd ./modpacks/simply-legacy/cf/yarn/1.21.10-fabric && packwiz refresh -y) &
-(cd ./modpacks/re-console-plus/mr/yarn/1.21.10-fabric && packwiz refresh -y) &
-(cd ./modpacks/re-console-plus/cf/yarn/1.21.10-fabric && packwiz refresh -y) &
+(cd ./modpacks/simply-legacy/mr/yarn && pw batch refresh -y) &
+(cd ./modpacks/simply-legacy/cf/yarn && pw batch refresh -y) &
+(cd ./modpacks/re-console-plus/mr/yarn && pw batch refresh -y) &
+(cd ./modpacks/re-console-plus/cf/yarn && pw batch refresh -y) &
 (cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge && pw batch refresh -y) &
 (cd ./modpacks/rekindled-legacy/cf/forged/1.21.1-neoforge && pw batch refresh -y) &
 wait
