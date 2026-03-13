c0962c0348deb70fe810fdd54c82abbed8ac9fa7
omo50 <144749186+omo50@users.noreply.github.com>
2026-03-13 14:02:01 -0600

    chore(actions): m,ake all use pw wrapper

--- update-all.sh
+++ update-all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Updating
-(cd ./modpacks/simply-legacy/mr/yarn/1.21.10-fabric && packwiz update -a -y) &
-(cd ./modpacks/simply-legacy/cf/yarn/1.21.10-fabric && packwiz update -a -y) &
-(cd ./modpacks/re-console-plus/mr/yarn/1.21.10-fabric && packwiz update -a -y) &
-(cd ./modpacks/re-console-plus/cf/yarn/1.21.10-fabric && packwiz update -a -y) &
+(cd ./modpacks/simply-legacy/mr/yarn && pw batch update -a -y) &
+(cd ./modpacks/simply-legacy/cf/yarn && pw batch update -a -y) &
+(cd ./modpacks/re-console-plus/mr/yarn && pw batch update -a -y) &
+(cd ./modpacks/re-console-plus/cf/yarn && pw batch update -a -y) &
 (cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge && pw batch update -a -y) &
 (cd ./modpacks/rekindled-legacy/cf/forged/1.21.1-neoforge && pw batch update -a -y) &
 wait
