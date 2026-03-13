a4f50a9456be55c41cc924279ecb25b7829003c4
omo50 <144749186+omo50@users.noreply.github.com>
2026-03-13 13:53:06 -0600

    chore(actions): improve auto update and auto refresh to use pw wrapper

--- update-all.sh
+++ update-all.sh
@@ -2,16 +2,9 @@
 echo Updating
 (cd ./modpacks/simply-legacy/mr/yarn/1.21.10-fabric && packwiz update -a -y) &
 (cd ./modpacks/simply-legacy/cf/yarn/1.21.10-fabric && packwiz update -a -y) &
-(cd ./modpacks/re-console+/mr/yarn/1.21.10-fabric && packwiz update -a -y) &
-(cd ./modpacks/re-console+/cf/yarn/1.21.10-fabric && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0033 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0035 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0054 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/gdc2012 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu0 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu1 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu2 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu3 && packwiz update -a -y) &
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu5 && packwiz update -a -y) &
+(cd ./modpacks/re-console-plus/mr/yarn/1.21.10-fabric && packwiz update -a -y) &
+(cd ./modpacks/re-console-plus/cf/yarn/1.21.10-fabric && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge && pw batch update -a -y) &
+(cd ./modpacks/rekindled-legacy/cf/forged/1.21.1-neoforge && pw batch update -a -y) &
 wait
 echo Done
