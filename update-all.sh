44bd9037c815b9df1ec9b2295c051815d2f27335
omo50 <144749186+omo50@users.noreply.github.com>
2026-03-13 13:40:04 -0600

    chore(actions): add simply auto publish, builds, separate auto refresh and auto update
    this is my last commit b4 going on an extended break for personal stuff

--- update-all.sh
+++ update-all.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 echo Updating
-(cd ./modpacks/simply-legacy/mr/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/simply-legacy/cf/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/re-console+/mr/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/re-console+/cf/yarn/1.21.10-fabric && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0033 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0035 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0054 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/gdc2012 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu0 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu1 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu2 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu3 && packwiz refresh -y && packwiz update -a -y)
-(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu5 && packwiz refresh -y && packwiz update -a -y)
+(cd ./modpacks/simply-legacy/mr/yarn/1.21.10-fabric && packwiz update -a -y) &
+(cd ./modpacks/simply-legacy/cf/yarn/1.21.10-fabric && packwiz update -a -y) &
+(cd ./modpacks/re-console+/mr/yarn/1.21.10-fabric && packwiz update -a -y) &
+(cd ./modpacks/re-console+/cf/yarn/1.21.10-fabric && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0033 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0035 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/b0054 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/gdc2012 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu0 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu1 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu2 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu3 && packwiz update -a -y) &
+(cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge/tu5 && packwiz update -a -y) &
+wait
+echo Done
