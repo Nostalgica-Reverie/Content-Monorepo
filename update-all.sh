fbecf999a57ffef6a1dbd4c2c2b69e22ea185a19
omo50 <omo50@noreply.nostalgica.net>
2026-03-14 03:29:27 +0000

    chore(actions): add 2k to auto update

--- update-all.sh
+++ update-all.sh
@@ -4,6 +4,8 @@ echo Updating
 (cd ./modpacks/simply-legacy/cf/yarn && pw batch update -a -y) &
 (cd ./modpacks/re-console-plus/mr/yarn && pw batch update -a -y) &
 (cd ./modpacks/re-console-plus/cf/yarn && pw batch update -a -y) &
+(cd ./modpacks/2000s-edition/cf/yarn && pw batch update -a -y) &
+(cd ./modpacks/2000s-edition/mr/yarn && pw batch update -a -y) &
 (cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge && pw batch update -a -y) &
 (cd ./modpacks/rekindled-legacy/cf/forged/1.21.1-neoforge && pw batch update -a -y) &
 wait
