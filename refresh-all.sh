df0c2aff2d2a191b995f6563ea22afb20f6a65a0
omo50 <omo50@noreply.nostalgica.net>
2026-03-14 03:28:43 +0000

    chore(actions): add 2k to auto refresh

--- refresh-all.sh
+++ refresh-all.sh
@@ -4,6 +4,8 @@ echo Refreshing
 (cd ./modpacks/simply-legacy/cf/yarn && pw batch refresh -y) &
 (cd ./modpacks/re-console-plus/mr/yarn && pw batch refresh -y) &
 (cd ./modpacks/re-console-plus/cf/yarn && pw batch refresh -y) &
+(cd ./modpacks/2000s-edition/cf/yarn && pw batch refresh -y) &
+(cd ./modpacks/2000s-edition/mr/yarn && pw batch refresh -y) &
 (cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge && pw batch refresh -y) &
 (cd ./modpacks/rekindled-legacy/cf/forged/1.21.1-neoforge && pw batch refresh -y) &
 wait
