3dbb3695f3961694988efc475748cb68c5e1492a
omo50 <144749186+omo50@users.noreply.github.com>
2026-03-14 21:29:11 -0600

    chore(actions): comment out simply and rc in auto update

--- update-all.sh
+++ update-all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo Updating
-(cd ./modpacks/simply-legacy/mr/yarn && pw batch update -a -y) &
-(cd ./modpacks/simply-legacy/cf/yarn && pw batch update -a -y) &
-(cd ./modpacks/re-console-plus/mr/yarn && pw batch update -a -y) &
-(cd ./modpacks/re-console-plus/cf/yarn && pw batch update -a -y) &
+# (cd ./modpacks/simply-legacy/mr/yarn && pw batch update -a -y) &
+# (cd ./modpacks/simply-legacy/cf/yarn && pw batch update -a -y) &
+# (cd ./modpacks/re-console-plus/mr/yarn && pw batch update -a -y) &
+# (cd ./modpacks/re-console-plus/cf/yarn && pw batch update -a -y) &
 (cd ./modpacks/2000s-edition/cf/yarn && pw batch update -a -y) &
 (cd ./modpacks/2000s-edition/mr/yarn && pw batch update -a -y) &
 (cd ./modpacks/rekindled-legacy/mr/forged/1.21.1-neoforge && pw batch update -a -y) &
