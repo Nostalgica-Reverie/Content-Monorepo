4dc7d2e2608d313cd95f6aecf79f2969b0f5cdc5
omo50 <144749186+omo50@users.noreply.github.com>
2026-04-14 21:58:41 -0600

    chore(template): init

---
+++ tools/templates/template-unfinished.rb
@@ -0,0 +1,21 @@
+{
+  id: "rc-plus", # project folder
+  name: "Re-Console Plus", # project name
+  type: "modpack", # this could be resourcepack, datapack, or modpack
+  
+  version: "26.04.1", 
+  
+# this will be the order versions are published in, so set it up properly. version is equal to folder name
+  mc_versions: [
+    "1.21.11-mr", 
+    "1.21.11-cf", 
+    "1.21.10-mr",
+    "1.21.10-cf"
+  ],
+# your page ids on mr and cf
+  modrinth_id: "legacy-minecraft",
+  curseforge_id: "re-console",
+
+  author: "omo50, Cjnator38, Technocality",
+  license: "GPL-3.0"
+}
